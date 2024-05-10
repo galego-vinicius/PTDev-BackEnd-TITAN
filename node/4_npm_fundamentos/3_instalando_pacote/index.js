ac0ec29ca88d4e774f784506347b4cfdde826c0a
vinicius.felix <vfelix1998@gmail.com>
2024-05-10 13:15:21 -0300

    add: iniciando fundamentos npm

---
+++ node/4_npm_fundamentos/3_instalando_pacote/index.js
@@ -0,0 +1,9 @@
+const _ = require('lodash') // terminal npm install lodash
+
+const a = [1, 2, 3, 4, 5]
+const b = [2, 4, 6, 7, 8]
+
+const diff = _.difference(a,b) //Diferença entre as duas arrays
+
+console.log(diff)
+
